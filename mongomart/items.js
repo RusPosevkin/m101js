94bb38939910056170fa8ad1ab429c34312959c8
rusposevkin <rus_posevkin@mail.ru>
2016-05-02 14:24:26 +0300

    lab 4

--- mongomart/items.js
+++ mongomart/items.js
@@ -337,13 +337,21 @@ function ItemDAO(database) {
 
         // TODO replace the following two lines with your code that will
         // update the document with a new review.
-        var doc = this.createDummyItem();
-        doc.reviews = [reviewDoc];
+        // var doc = this.createDummyItem();
+        // doc.reviews = [reviewDoc];
+
+        this.db.collection("item").updateOne(
+            { _id : itemId },
+            { $push: { "reviews": reviewDoc } },
+        function(err, results) {
+            assert.equal(null, err);
+            callback(results);
+        });
 
         // TODO Include the following line in the appropriate
         // place within your code to pass the updated doc to the
         // callback.
-        callback(doc);
+        // callback(doc);
     }
 
 
